fea6d41dcfc13e642b36feb6142deaf178a1258e
Mostafa Hiebah <mhiebah@gmail.com>
2024-08-27 12:23:01 +0300

    Update and rename 02Loop.sh to Loop.sh

--- Loop.sh
+++ Loop.sh
@@ -1,18 +1,25 @@
 #! /bin/bash
-
 for i in {1..10}
 do
 echo "Number is $i"
 done
 
+
+
 #!/bin/bash  
 #This is the basic example of 'for loop'.  
-  
 learn="Start learning from Javatpoint."  
   echo
 for learn in $learn  
 do  
 echo $learn  
 done  
-  
 echo "Thank You."  
+
+
+x=`ls`
+for i in $x
+do
+    echo file name is: $i
+    cat $i
+done
